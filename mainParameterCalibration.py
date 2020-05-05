e1f75cda3580fc741d6c7ce44ca11a75cbd3d7cf
Gabriel de Castro Michelassi <gabrielmichelassi@usp.br>
2020-05-05 13:32:08 -0300

    Adiciona teste nro 6

--- mainParameterCalibration.py
+++ mainParameterCalibration.py
@@ -165,5 +165,5 @@ def run_randomizedSearch(dataset='euclidian_px_all', filtro=0.0):
 
 if __name__ == '__main__':
     start_time = time.time()
-    run_gridSearch(amostragem='SVM')
+    run_gridSearch(amostragem='random')
     log.info("--- Total execution time: %s minutes ---" % ((time.time() - start_time) / 60))
