fce9d2859b607237b59e16f928162d132d9d8b45
Gabriel de Castro Michelassi <gabrielmichelassi@usp.br>
2020-07-09 11:35:59 -0300

    Limpa repositório output

--- mainParameterCalibration.py
+++ mainParameterCalibration.py
@@ -112,7 +112,8 @@ def run_gridSearch(lib='dlibHOG', dataset='euclidian_px_all'):
 
     isRandomForestDone = False
     dimensionality_reductions = ['None', 'PCA', 'mRMR', 'FCBF', 'CFS', 'RFS', 'ReliefF']
-    classifiers = {'randomforestclassifier': rf, 'svc': svm, 'kneighborsclassifier': knn, 'mlpclassifier': nnn, 'gaussiannb': nb}
+    # 'randomforestclassifier': rf, 'svc': svm,
+    classifiers = {'kneighborsclassifier': knn, 'mlpclassifier': nnn, 'gaussiannb': nb}
     amostragens = [None, 'Random', 'Smote', 'Borderline', 'KMeans', 'SVM', 'Tomek']
     filtros = [0.0, 0.98, 0.99]
     min_maxs = [False, True]
