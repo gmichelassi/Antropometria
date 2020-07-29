f51063f758a04e3f13024ae65e2c4a355fd36f6b
Gabriel de Castro Michelassi <gabrielmichelassi@usp.br>
2020-07-29 13:40:16 -0300

    .

--- mainParameterCalibration.py
+++ mainParameterCalibration.py
@@ -229,5 +229,16 @@ def run_randomizedSearch(dataset='distances_all_px_eu', filtro=0.0):
 
 if __name__ == '__main__':
     start_time = time.time()
-    runGridSearch(lib='ratio', dataset='distances_all_px_eu')
+    X = pd.read_csv('./data/dlibHOG_semfaixa/casos_distances_all_px_eu.csv')
+    X = X.drop('img_name', axis=1)
+    X = X.drop('id', axis=1)
+    build_ratio_dataset(X, 'casos')
+    X = pd.read_csv('./data/dlibHOG_semfaixa/controles_distances_all_px_eu.csv')
+    X = X.drop('img_name', axis=1)
+    X = X.drop('id', axis=1)
+    build_ratio_dataset(X, 'controles')
+    X = None
+
+    # runGridSearch(lib='ratio', dataset='distances_all_px_eu')
+
     log.info("--- Total execution time: %s minutes ---" % ((time.time() - start_time) / 60))
