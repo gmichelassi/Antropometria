375f60195ec92062b791dd0eb30daf204549a5ed
Gabriel de Castro Michelassi <gabrielmichelassi@usp.br>
2020-08-15 14:54:56 -0300

    Adiciona tratamento para aplicar a correlação no dataset gigante

--- mainSplitDataFrame.py
+++ mainSplitDataFrame.py
@@ -1,7 +1,7 @@
 import asd_data as asd
 
 # Utils
-from utils.utils import apply_pearson_feature_selection
+from utils.utils import apply_pearson_feature_selection, build_ratio_dataset
 from scipy import stats
 import os
 import time
@@ -21,7 +21,7 @@ def splitDataFrame():
     #  https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.split.html
     #  https://docs.scipy.org/doc/numpy/reference/generated/numpy.arange.html
 
-    X, y = asd.load_data(lib='ratio', dataset='distances_all_px_eu', classes=['tea', 'apert', 'down', 'controles'], verbose=True)
+    X, y = asd.load_data(lib='ratio', dataset='distances_all_px_eu', classes=['casos', 'controles'], verbose=True)
 
     N = 33780
     log.info("Splitting dataset")
@@ -109,6 +109,18 @@ def custom_pearson_feature_selection(samples, max_value=0.99, where_to_start=1):
 
 
 if __name__ == '__main__':
+    X = pd.read_csv('./data/dlibHOG/casos_distances_all_px_eu.csv')
+    X = X.drop('img_name', axis=1)
+    X = X.drop('id', axis=1)
+    build_ratio_dataset(X, 'casos')
+
+    X = pd.read_csv('./data/dlibHOG/controles_distances_all_px_eu.csv')
+    X = X.drop('img_name', axis=1)
+    X = X.drop('id', axis=1)
+    build_ratio_dataset(X, 'controles')
+
+    splitDataFrame()
+
     proc1 = Process(target=runPearsonCorrelation, args=(0, 7, False, 0))
     proc2 = Process(target=runPearsonCorrelation, args=(8, 15, False, 0))
     proc3 = Process(target=runPearsonCorrelation, args=(16, 23, False, 0))
