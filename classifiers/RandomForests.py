8a66f14a193e12d51e4dad1128f01e26d4afd250
Gabriel <gabrielmichelassi@usp.br>
2021-06-13 11:52:11 -0300

    refactor: changed n_features to sqrt_n_features to clarify its usage on RF, simplify requirements.txt

--- classifiers/RandomForests.py
+++ classifiers/RandomForests.py
@@ -5,16 +5,16 @@ from sklearn.ensemble import RandomForestClassifier
 class RandomForests:
     def __init__(self, name: str = 'RandomForestClassifier', n_features: int = 0):
         self.name = name
-        self.n_features = int(sqrt(n_features))
+        self.sqrt_n_features = int(sqrt(n_features))
         self.estimator = RandomForestClassifier()
         self.parameter_grid = {
             'n_estimators': [500, 1000, 1500, 2000],
             'criterion': ['gini'],
             'max_depth': [None],
             'min_samples_leaf': [1, 2, 5, 10, 15, 20],
-            'max_features': [i for i in range(int(0.5 * n_features),
-                                              6 * n_features,
-                                              int(((6 * n_features - 0.5 * n_features) / 50))
+            'max_features': [i for i in range(int(0.5 * self.sqrt_n_features),
+                                              6 * self.sqrt_n_features,
+                                              int(((6 * self.sqrt_n_features - 0.5 * self.sqrt_n_features) / 50))
                                               )],
             'bootstrap': [True],
             'n_jobs': [-1],
