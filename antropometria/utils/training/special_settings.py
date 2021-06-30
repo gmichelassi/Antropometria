d1310d258b4a48c4b8bce10dcfea9f0cad5a2905
Gabriel <gabrielmichelassi@usp.br>
2021-06-29 21:19:55 -0300

    feat: refactored tests from special settings (for now just cases that needs to stop the main pipeline)

---
+++ antropometria/utils/training/special_settings.py
@@ -0,0 +1,30 @@
+from typing import Any, Union
+from antropometria.classifiers.RandomForests import RandomForests
+
+
+def stop_running_rf(
+        is_random_forest_done: bool = False,
+        classifier_name: str = 'Any',
+        reduction: Union[str, None] = 'Any'
+) -> bool:
+    if not is_random_forest_done and classifier_name == 'RandomForestClassifier' and reduction is None:
+        return False
+    if not is_random_forest_done and classifier_name == 'RandomForestClassifier' and reduction == 'RFSelect':
+        return True
+    if is_random_forest_done:
+        return True
+
+    return False
+
+
+def skip_current_test(
+        is_random_forest_done: bool = False,
+        classifier: Any = 'Any',
+        reduction: Union[str, None] = 'Any'
+) -> bool:
+    if is_random_forest_done and classifier == RandomForests:
+        return True
+    if classifier != RandomForests and reduction is None:
+        return True
+
+    return False
