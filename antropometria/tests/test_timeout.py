eb57f74a3bc5944bd2ce4b620d8523dfe7bcda4d
Gabriel <gabrielmichelassi@usp.br>
2021-07-07 22:47:11 -0300

    feat: added custom decorator to exit if a function takes more than N seconds to run

---
+++ antropometria/tests/test_timeout.py
@@ -0,0 +1,56 @@
+from context import set_tests_context
+set_tests_context()
+
+import pytest
+import time
+
+from antropometria.utils.timeout import timeout
+from pytest import approx
+from unittest.mock import MagicMock
+
+WAIT_TIME = 5
+MOCK = MagicMock()
+
+
+@timeout(WAIT_TIME, True)
+def this_function_will_take_time(time_to_wait):
+    time.sleep(time_to_wait)
+    MOCK()
+
+
+@timeout(WAIT_TIME, False)
+def this_function_will_be_called_instantly():
+    MOCK()
+
+
+class TestTimeout:
+    def test_function_is_not_called_when_time_expire(self):
+        MOCK.reset_mock()
+        start_time = time.time()
+        with pytest.raises(TimeoutError):
+            this_function_will_take_time(WAIT_TIME + 1)
+
+        ellapsed_time = time.time() - start_time
+
+        assert not MOCK.called
+        assert approx(ellapsed_time, abs=0.11) == WAIT_TIME
+
+    def test_function_is_called_when_time_does_not_expire(self):
+        MOCK.reset_mock()
+        start_time = time.time()
+        this_function_will_take_time(WAIT_TIME - 1)
+
+        ellapsed_time = time.time() - start_time
+
+        assert approx(ellapsed_time, abs=0.11) == WAIT_TIME - 1
+        assert MOCK.called
+
+    def test_timeout_is_not_applied_if_condition_is_not_matched(self):
+        MOCK.reset_mock()
+        start_time = time.time()
+        this_function_will_be_called_instantly()
+
+        ellapsed_time = time.time() - start_time
+
+        assert approx(ellapsed_time, abs=0.11) == 0
+        assert MOCK.called
---
+++ antropometria/utils/timeout.py
@@ -0,0 +1,21 @@
+import signal
+
+from typing import Callable
+
+
+def timeout(seconds: int, use_timeout: bool):
+    def decorator(function: Callable):
+        if not use_timeout:
+            return function
+
+        def wrapper(args, **kwargs):
+            def handler(signum, frame):
+                raise TimeoutError(f'This call took longer than {seconds}')
+
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(seconds)
+            result = function(args, **kwargs)
+            signal.alarm(0)
+            return result
+        return wrapper
+    return decorator
