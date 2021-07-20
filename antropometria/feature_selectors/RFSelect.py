f74cfc1f0eff8ac439fc36720e6aa07084bb8d8e
Gabriel <gabrielmichelassi@usp.br>
2021-07-19 21:43:55 -0300

    refactor: removed multiple ifs from getter of feature selectors

--- antropometria/feature_selectors/RFSelect.py
+++ antropometria/feature_selectors/RFSelect.py
@@ -6,13 +6,12 @@ from sklearn.feature_selection import SelectFromModel
 
 
 class RFSelect(BaseEstimator, TransformerMixin):
-    def __init__(self):
+    def __init__(self, n_features_to_select: int):
         self.__sel = SelectFromModel(RandomForestClassifier())
+        self.n_features_to_select = n_features_to_select
 
-    def fit(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def fit(self, x: np.ndarray, y: np.ndarray):
         self.__sel.fit(x, y)
-
-        return self
 
     def transform(self, x: np.ndarray) -> np.ndarray:
         return x[:, self.__sel.get_support()]
--- antropometria/feature_selectors/utils/getter.py
+++ antropometria/feature_selectors/utils/getter.py
@@ -13,26 +13,33 @@ from skrebate import ReliefF
 from typing import Union
 
 
+REDUCTIONS = {
+    'pca': PCA,
+    'mrmr': MRMR,
+    'fcbf': FCBF,
+    'cfs': CFS,
+    'rfs': RFS,
+    'relieff': ReliefF,
+    'rfselect': RFSelect,
+}
+
+
 def get_feature_selector(
         reduction: str,
         n_features_to_keep: int,
         instances: int,
         features: int
 ) -> Union[CFS, FCBF, MRMR, RFS, RFSelect, PCA, ReliefF]:
-    if reduction == 'PCA':
+    lowered_reduction = reduction.lower()
+    reduction_method = REDUCTIONS[lowered_reduction]
+
+    if lowered_reduction == 'pca':
         if n_features_to_keep < min(instances, features):
-            return PCA(n_components=n_features_to_keep)
+            return reduction_method(n_components=n_features_to_keep)
         else:
-            return PCA(n_components=min(instances, features))
-    elif reduction == 'mRMR':
-        return MRMR(n_features_to_select=n_features_to_keep)
-    elif reduction == 'FCBF':
-        return FCBF(n_features_to_select=n_features_to_keep)
-    elif reduction == 'CFS':
-        return CFS(n_features_to_select=n_features_to_keep)
-    elif reduction == 'RFS':
-        return RFS(n_features_to_select=n_features_to_keep)
-    elif reduction == 'ReliefF':
-        return ReliefF(n_features_to_select=n_features_to_keep, n_neighbors=100, n_jobs=-1)
-    elif reduction == 'RFSelect':
-        return RFSelect()
+            return reduction_method(n_components=min(instances, features))
+
+    if lowered_reduction == 'relieff':
+        return reduction_method(n_features_to_select=n_features_to_keep, n_neighbors=100, n_jobs=-1)
+
+    return reduction_method(n_features_to_keep)
