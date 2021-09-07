291a33f12a7d26f8d6d3515214c619b928968bb9
Gabriel <gabrielmichelassi@usp.br>
2021-09-07 10:20:42 -0300

    release: run rf

--- antropometria/mainParameterCalibration.py
+++ antropometria/mainParameterCalibration.py
@@ -20,7 +20,7 @@ from utils.parameter_calibration.to_dict import test_to_dict, grid_search_result
 log = logger.get_logger(__file__)
 initial_context.set_context()
 
-CLASSIFIERS = [Knn]
+CLASSIFIERS = [Rf]
 
 
 def run_grid_search(
