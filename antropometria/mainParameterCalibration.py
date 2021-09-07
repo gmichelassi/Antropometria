8cbdeeffd88c3d693897d001e1d12af274df87bc
Gabriel <gabrielmichelassi@usp.br>
2021-09-07 10:18:00 -0300

    release: run knn

--- antropometria/mainParameterCalibration.py
+++ antropometria/mainParameterCalibration.py
@@ -20,7 +20,7 @@ from utils.parameter_calibration.to_dict import test_to_dict, grid_search_result
 log = logger.get_logger(__file__)
 initial_context.set_context()
 
-CLASSIFIERS = [Nb]
+CLASSIFIERS = [Knn]
 
 
 def run_grid_search(
