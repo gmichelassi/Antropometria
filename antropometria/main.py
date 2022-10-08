dc26d6e6f5080c3c7a2002302b9134929088c720
Gabriel Michelassi <gabrielmichelassi10@hotmail.com>
2022-10-07 21:00:00 -0300

    feat: added processed folder setup

--- antropometria/main.py
+++ antropometria/main.py
@@ -2,6 +2,7 @@ import time
 
 from antropometria.config import get_logger
 from antropometria.hyperparameter_tuning import run_hyperparameter_tuning
+from antropometria.preprocessing.run_preprocessing import run_preprocessing
 from antropometria.utils.load_data import LoadData
 from typing import List
 
@@ -16,10 +17,6 @@ def main(folder: str, dataset_name: str, classes: List[str]):
 
     data = LoadData(folder, dataset_name, classes).load()
 
-    run_hyperparameter_tuning('shared_instances', 'mediapipe64', ['single'])
-    run_hyperparameter_tuning('shared_instances', 'opencvdnn', ['single'])
-    run_hyperparameter_tuning('shared_instances', 'opencvhaar', ['single'])
-    run_hyperparameter_tuning('shared_instances', 'openface', ['single'])
-    run_hyperparameter_tuning('shared_instances', 'mediapipecustom', ['single'])
+    run_preprocessing(data)
 
     log.info("--- Total execution time: %s minutes ---" % ((time.time() - start_time) / 60))
--- antropometria/preprocessing/run_preprocessing.py
+++ antropometria/preprocessing/run_preprocessing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 from .preprocess import preprocess
 from antropometria.config import FILTERS, MIN_MAX_NORMALIZATION, REDUCTIONS, SAMPLINGS
@@ -8,8 +9,16 @@ from typing import Tuple
 
 
 PREPROCESSING_PARAMS = product(REDUCTIONS, SAMPLINGS, FILTERS, MIN_MAX_NORMALIZATION)
+PROCESSED_DIR = 'antropometria/data/processed/'
 
 
 def run_preprocessing(data: Tuple[DataFrame, np.ndarray]):
+    setup_processed_directory()
+
     for reduction, sampling, p_filter, apply_min_max in PREPROCESSING_PARAMS:
         x, y = preprocess(data, apply_min_max, p_filter, reduction, sampling)
+
+
+def setup_processed_directory():
+    if not os.path.exists(PROCESSED_DIR):
+        os.mkdir(PROCESSED_DIR)
